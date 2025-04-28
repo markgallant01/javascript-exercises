34e1909dd81cd09efbf72849b90703817bf38130
markgallant01 <markgallant01@gmail.com>
2025-04-28 02:29:58 -0400

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+  let result = arr;
+  for (let i = 1; i < arguments.length; i++) {
+    result = result.filter(element => element !== arguments[i]);
+  }
+  return result;
 };
 
 // Do not edit below this line
