e7d31b5cc0c750925b039a70876567a85cb8dc1f
markgallant01 <markgallant01@gmail.com>
2025-04-28 02:05:37 -0400

    complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeatCount) {
+  if (repeatCount < 0) {
+    return "ERROR";
+  }
 
+  let repeatedString = "";
+  for (let i = 0; i < repeatCount; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
