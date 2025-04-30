db070b36bf5f3612aa56bb53ed61e92ea4ef1d04
markgallant01 <markgallant01@gmail.com>
2025-04-29 20:09:59 -0400

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const badChars = ".,/?! ";
+  
+  word = word.toLowerCase();
+  let letters = word.split("");
+  letters = letters.filter((letter) => !(badChars.includes(letter)));
+  let originalWord = letters.join("");
 
+  letters.reverse();
+  let reversedWord = letters.join("");
+  return originalWord === reversedWord;
 };
 
 // Do not edit below this line
