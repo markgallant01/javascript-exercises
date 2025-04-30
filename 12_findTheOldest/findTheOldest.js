a386b134758d94a132b0dd0030db1dc7e2fb0aaa
markgallant01 <markgallant01@gmail.com>
2025-04-29 20:39:58 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let oldestPerson = null;
+  
+  // grab the first object with a non-undefined yearOfDeath value
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].yearOfDeath !== undefined) {
+      oldestPerson = arr[i];
+      break;
+    }   
+  }
 
+  let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+
+  arr.forEach((person) => {
+    if (person.yearOfDeath === undefined) { return; }
+    let personAge = person.yearOfDeath - person.yearOfBirth;
+    if (personAge > oldestAge) {
+      oldestPerson = person;
+      oldestAge = personAge;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
